f9d588c73190171ac0a602086499295d577f6257
Andres <67043785+GuppyPaw@users.noreply.github.com>
2021-09-09 15:48:26 -0500

    Code read files

--- main.cpp
+++ main.cpp
@@ -3,12 +3,42 @@
 //A01638129 Andres Olvera Rodriguez
 //03 de Septiembre del 2021
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//
+
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include <string>
+
 using namespace std;
-int main(){
+
+void leeArchivo(const char* nombreArchivo)
+{
+    ifstream inFile;
+    char x;
+
+    inFile.open(nombreArchivo);
+
+    if(!inFile)
+    {
+        cerr << "No se abrio el archivo";
+        exit(1);
+    }
+
+    while(inFile >> x)
+    {
+        cout << x;
+    }
+    cout << endl;
+    cout << endl;
+
+    inFile.close();
+}
+
+int main()
+{
+    leeArchivo("mcode1.txt");
+    leeArchivo("mcode2.txt");
+    leeArchivo("mcode3.txt");
 
     system("pause");
 }
