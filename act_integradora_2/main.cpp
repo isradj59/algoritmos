346eca38fffd90874643834d2a9d2223a26e9e8c
oscar-me <oscar-me@outlook.com>
2021-10-11 11:55:03 -0500

    begin act_integradora_2

---
+++ act_integradora_2/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * Actividad integradora 2
+ *
+ * A01638258 Enrique Israel Delgado Joaquin
+ * A01638129 Andres Olvera Rodriguez
+ * A01630791 Oscar Miranda Escalante
+ *
+ * 09 de octubre de 2021
+ * Análisis y diseño de algoritmos avanzados
+ * */
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  return 0;
+}
